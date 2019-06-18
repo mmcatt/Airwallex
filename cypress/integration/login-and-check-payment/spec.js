efa9e6ddad86add9a278a468c4de23f140fe1fcd
cynthia <mmcatt@qq.com>
2019-06-18 21:57:22 +0800

    Add test: Log in and check payment details

---
+++ cypress/integration/login-and-check-payment/spec.js
@@ -0,0 +1,25 @@
+it('Log in and check payment details', () => {
+  cy.readFile(Cypress.env('zh'), 'utf8').then(($lang) => {
+    cy.visit($lang.root)
+    cy.enterLoginPage($lang.login)
+    cy.readFile(Cypress.env('user')).then(($user) => {
+      cy
+        .login('#email', $user.username, '#password', $user.password, $lang.loginForm, $lang.login)
+        .wait(1000)
+      cy.url().should('include',$lang.dashboard)
+      cy.get($lang.primary_nav_payments).click()
+      cy.get($lang.payment_activity_tab).click()
+      cy.get('table').contains('tr',$lang.paymentamount).should('be.visible')
+      cy.contains($lang.filters).click()
+      cy.get($lang.paymentId).should('be.visible').wait(500).type($user.paymentid)
+      cy.get($lang.footer_links).contains($lang.applyfilters).click()
+      cy.get('tbody>tr>td').eq(1).click()
+      cy.checkValue($lang.type_value, $lang.description_value, $lang.payment)
+      cy.checkValue($lang.status_value, $lang.description_value, $lang.dispatched)
+      cy.checkValue($lang.account_id_value, $lang.description_value, $user.accountid)
+      cy.checkValue($lang.payment_id_value, $lang.description_value, $user.paymentid)
+      cy.checkValue($lang.request_id_value, $lang.description_value, $user.requestid)
+
+    })
+  })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This is will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('enterLoginPage', (loginflag) => {
+    cy.contains(loginflag).click()
+  })
+Cypress.Commands.add('login', (email, usernameV, password, passwordV, loginForm, loginText) => {
+    cy.get(email).should('be.visible').wait(1000).type(usernameV)
+    cy.get(password).should('be.visible').wait(500).type(passwordV)
+    cy.get(loginForm).contains(loginText).click()
+  })
+Cypress.Commands.add('checkValue', (value, valueText, expectText) => {
+    cy.get(value)
+      .find(valueText)
+      .should('have.text',expectText)
+  })
